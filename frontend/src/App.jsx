53859d474f436dd0275f92b225494986fb7bbdf1
JaCaria Conway <jacariaconway@gmail.com>
2026-01-27 23:00:59 -0500

    First Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import MovieCard from "./components/MovieCard"
+
+function App() {
+  return (
+    <> 
+      <MovieCard movie={{title: "Jae's Film", release_date: "2026"}}/>
+    </> 
+  )
+}
+
+export default App
